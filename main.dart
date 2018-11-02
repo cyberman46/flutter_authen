2cc1d978091668f90a96f828aef5be82358cf323
akekapon <akekapon27@gmail.com>
2018-11-02 09:57:42 +0700

    first

---
+++ main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async';
+//import 'package:';
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatelessWidget{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+
+  Future<FirebaseUser> _signIn() async{
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+
+    FirebaseUser user = await _auth.signInWithGoogle(
+        idToken: gSA.idToken, accessToken: gSA.accessToken);
+    print("User Name : ${user.displayName}");
+    return user;
+  }
+
+
+  void _signOut(){
+
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Firebase Demo"),
+      ),
+      body: new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new RaisedButton(
+              onPressed: ()=> _signIn()
+                  .then((FirebaseUser user) => print(user))
+                  .catchError((e) => print(e)),
+              child: new Text("Sign In"),
+              color: Colors.green,
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            new RaisedButton(
+              onPressed: ()=> _signOut(),
+              child: new Text("sign Out"),
+              color: Colors.red,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
